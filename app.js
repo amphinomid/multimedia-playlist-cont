23ecfb7ac87359aea6ea65904a5e032f53782f33
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-06-07 21:44:29 -0500

    update

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ var summer_tl = gsap.timeline( { defaults: { duration: 16 },
         scrub: 1,
         pin: true,
         start: 'top top',
-        end: '+=5000px'
+        end: '+=2730px'
     }
 })
 .to('.summer-songs', {
     motionPath: {
-        path: [ {x: -5130 + document.documentElement.clientWidth, y: 0} ]
+        path: [ {x: -2730 + document.documentElement.clientWidth, y: 0} ]
     },
     duration: 13,
     ease: 'power3.inOut'
@@ -43,14 +43,9 @@ var summer_tl = gsap.timeline( { defaults: { duration: 16 },
 .to('.summer-background', { background: "linear-gradient(90deg, hsla(222, 40%, 50%, 1) 0%, hsla(214, 39%, 53%, 1) 50%, hsla(264, 36%, 43%, 1) 100%)", duration: 1.25 }, '5.25')
 .to('.summer-6', { opacity: 0, duration: 1.25 }, '6')
 .to('.summer-animation', { background: "#000000", duration: 1 }, '6')
-.to('.icantbreathe-her-lyrics', { color: "#FFFFFF", duration: 0.25 }, '6')
-.to('.pporappippam-sunmi-lyrics1', { color: "#FFFFFF", duration: 0.25 }, '6')
-.to('.pporappippam-sunmi-lyrics2', { color: "#FFFFFF", duration: 0.25}, '6')
-.to('.freesomebody-luna-lyrics', { color: "#FFFFFF", duration: 0.25}, '6')
 .to('.summer-background', { background: "linear-gradient(90deg, hsla(222, 34%, 25%, 1) 0%, hsla(213, 30%, 28%, 1) 50%, hsla(264, 30%, 23%, 1) 100%)", duration: 1.25 }, '6')
 .to('.summer-background', { background: "linear-gradient(90deg, hsla(222, 20%, 13%, 1) 0%, hsla(212, 23%, 14%, 1) 50%, hsla(265, 38%, 6%, 1) 100%)", duration: 1.25 }, '7.25')
 .to('.summer-background', { background: "linear-gradient(0deg, hsla(0, 0%, 0%, 1) 0%, hsla(0, 0%, 0%, 1) 50%, hsla(0, 0%, 0%, 1) 100%)", duration: 5.5 }, '8.5')
-//.to('.summer-7', { opacity: 0, duration: 2 }, '14')
 
 var autumn_tl = gsap.timeline( { defaults: { duration: 16 },
     scrollTrigger: {
@@ -58,12 +53,12 @@ var autumn_tl = gsap.timeline( { defaults: { duration: 16 },
         scrub: 1,
         pin: true,
         start: 'top top',
-        end: '+=5000px'
+        end: '+=2830px'
     }
 })
 .to('.autumn-songs', {
     motionPath: {
-        path: [ {x: -4790 + document.documentElement.clientWidth + 100, y: 0} ]
+        path: [ {x: -2830 + document.documentElement.clientWidth + 100, y: 0} ]
     },
     duration: 13,
     ease: 'power3.inOut'
@@ -97,11 +92,7 @@ var autumn_tl = gsap.timeline( { defaults: { duration: 16 },
 .to('.autumn-background', { background: "linear-gradient(90deg, hsla(0, 0%, 26%, 1) 0%, hsla(0, 0%, 33%, 1) 50%, hsla(0, 48%, 38%, 1) 100%)", duration: 1.25 }, '5.25')
 .to('.autumn-6', { opacity: 0, duration: 1.25 }, '6')
 .to('.autumn-animation', { background: "#000000", duration: 1 }, '6')
-.to('.miseducation-laurynhill-comments1', { color: "#FFFFFF", duration: 0.25 }, '6')
-.to('.miseducation-laurynhill-comments2', { color: "#FFFFFF", duration: 0.25 }, '6')
-.to('.inseoul-epikhigh-lyrics', { color: "#FFFFFF", duration: 0.25 }, '6')
 .to('.autumn-7', { opacity: 1, duration: 0.5 }, '6')
 .to('.autumn-background', { background: "linear-gradient(90deg, hsla(0, 0%, 21%, 1) 0%, hsla(0, 0%, 25%, 1) 50%, hsla(0, 58%, 15%, 1) 100%)", duration: 1.25 }, '6')
 .to('.autumn-background', { background: "linear-gradient(90deg, hsla(0, 0%, 16%, 1) 0%, hsla(0, 0%, 16%, 1) 50%, hsla(0, 74%, 9%, 1) 100%)", duration: 1.25 }, '7.25')
 .to('.autumn-background', { background: "linear-gradient(0deg, hsla(0, 0%, 0%, 1) 0%, hsla(0, 0%, 0%, 1) 50%, hsla(0, 0%, 0%, 1) 100%)", duration: 5.5 }, '8.5')
-//.to('.autumn-7', { opacity: 0, duration: 2 }, '14')
